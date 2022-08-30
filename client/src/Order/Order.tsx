6f52f64466f6197404d01cafe2fe66aeb9b23dfb
Ohad Hakim <hakimohad@gmail.com>
2022-08-30 19:41:19 +0300

    adding states and types

--- client/src/Order/Order.tsx
+++ client/src/Order/Order.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { CardType } from "../Card/Card";
 import Title from "../Title/Title";
@@ -5,11 +6,28 @@ import Title from "../Title/Title";
 interface LocationState {
   state: CardType;
 }
+
+type delivery = "takeaway" | "delivery";
+type payment = "cc" | "cash";
+
 function Order() {
   const location = useLocation();
   const LocationState = location as LocationState;
   const order = LocationState.state;
 
+  const [name, setName] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [deliveryMethod, setDeliveryMethod] = useState<delivery>("takeaway");
+  const [paymentMethod, setPaymentMethod] = useState<payment>("cc");
+
+  function updateDeliveryPrice(): number {
+    return deliveryMethod === "delivery" ? 2 : 0;
+  }
+
+  function calcTotal(): number {
+    return order.price + updateDeliveryPrice();
+  }
   return (
     <>
       <Title text={`Order: ${order.name}`}>
@@ -37,9 +55,76 @@ function Order() {
         </div>
 
         <hr />
-        <div>
-          <label>Price:</label>
-          {order.price}
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Name"
+              />
+              <input
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Address"
+              />
+              <input
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Phone"
+              />
+            </div>
+            <div className="col">
+              <select
+                value={deliveryMethod}
+                onChange={(e) => setDeliveryMethod(e.target.value as delivery)}
+                className="form-select"
+              >
+                <option value="takeaway">Takeaway</option>
+                <option value="delivery">Delivery</option>
+              </select>
+
+              <label className="form-label">Payment Method:</label>
+              <select
+                value={paymentMethod}
+                onChange={(e) => setPaymentMethod(e.target.value as payment)}
+                className="form-select"
+              >
+                <option value="cc">Credit Card</option>
+                <option value="cash">Cash</option>
+              </select>
+            </div>
+            <div className="col">
+              <div className="row">
+                <div className="col-4">
+                  <label className="form-label">Price:</label>
+                </div>
+                <div className="col-8">{order.price}</div>
+              </div>
+
+              <div className="row">
+                <div className="col-4">
+                  <label className="form-label">Delivery:</label>
+                </div>
+                <div className="col-8">{updateDeliveryPrice()}</div>
+              </div>
+
+              <div className="row">
+                <div className="col-4">
+                  <label className="form-label">Total:</label>
+                </div>
+                <div className="col-8">{calcTotal()}</div>
+              </div>
+
+              <button className="btn btn-primary btn-lg">Pay Now</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
